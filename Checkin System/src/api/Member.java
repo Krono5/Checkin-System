e29591334578d710d55fc22e2ec9af6bba16fc17
Tyler Storr <TyStorr@gmail.com>
2015-02-07 23:11:38 -0500

    Fixed GUI after last commit not displaying add member correctly. Changed Api calls to static, and kept the member class

--- Checkin System/src/api/Member.java
+++ Checkin System/src/api/Member.java
@@ -3,50 +3,56 @@ package api;
 import java.util.Date;
 
 public class Member {
-private String memberName;
-private Date dateOfBirth;
-private Integer oAID;
-private Integer bSAID;
+	private String memberName;
+	private Date dateOfBirth;
+	private Integer oAID;
+	private Integer bSAID;
 
-public Member(){
-	this.memberName = null;
-	this.dateOfBirth = null;
-	this.oAID = null;
-	this.bSAID = null;
-}
+	public Member() {
+		this.memberName = null;
+		this.dateOfBirth = null;
+		this.oAID = null;
+		this.bSAID = null;
+	}
 
-public Member(String memberName, Date dateOfBirth, Integer oAID, Integer bSAID){
-	memberName = this.memberName;
-	dateOfBirth = this.dateOfBirth;
-	oAID = this.oAID;
-	bSAID = this.bSAID;
-}
+	public Member(String memberName, Date dateOfBirth, Integer oAID,
+			Integer bSAID) {
+		memberName = this.memberName;
+		dateOfBirth = this.dateOfBirth;
+		oAID = this.oAID;
+		bSAID = this.bSAID;
+	}
 
-public String getMemberName() {
-	return memberName;
-}
-public void setMemberName(String memberName) {
-	this.memberName = memberName;
-}
-public Date getDateOfBirth() {
-	return dateOfBirth;
-}
-public void setDateOfBirth(Date dateOfBirth) {
-	this.dateOfBirth = dateOfBirth;
-}
-public Integer getoAID() {
-	return oAID;
-}
-public void setoAID(Integer oAID) {
-	this.oAID = oAID;
-}
-public Integer getbSAID() {
-	return bSAID;
-}
-public void setbSAID(Integer bSAID) {
-	this.bSAID = bSAID;
-}
+	public String getMemberName() {
+		return memberName;
+	}
 
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
 
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Integer getoAID() {
+		return oAID;
+	}
+
+	public void setoAID(Integer oAID) {
+		this.oAID = oAID;
+	}
+
+	public Integer getbSAID() {
+		return bSAID;
+	}
+
+	public void setbSAID(Integer bSAID) {
+		this.bSAID = bSAID;
+	}
 
 }
--- Checkin System/src/gui/GuiMain.java
+++ Checkin System/src/gui/GuiMain.java
@@ -22,13 +22,99 @@ public class GuiMain {
 		//Declaration of Frames and panels
 		JFrame frame = new JFrame();
 		JTabbedPane pane = new JTabbedPane();
-		
+		JPanel addMembers = new JPanel();
 		JPanel viewMembers = new JPanel();
 		JPanel editMembers = new JPanel();
 		JPanel deleteMembers = new JPanel();
 		
 		
 		
+		//Add Members
+			
+		JTabbedPane addMembersPane = new JTabbedPane();
+		JPanel personalPanel = new JPanel(new GridLayout(0,1));
+		JPanel paymentPanel = new JPanel(new GridLayout(0,1));
+		//Personal
+		JPanel buttonPanel = new JPanel();
+		
+		JLabel jobAssignmentLabel = new JLabel("Job Assignment");
+		JLabel sleepingLocationLabel = new JLabel ("Sleeping Location");
+		JLabel healthFormLabel = new JLabel ("Health Form");
+		JLabel verifyInfo = new JLabel ("Please verify your information");
+		JLabel dateOfBirthLabel = new JLabel("DOB");
+		JLabel emailAddressLabel = new JLabel("Email Address");
+		JLabel boyScoutIDLabel = new JLabel("BSA ID");
+		JLabel unitTypeLabel = new JLabel("Unit Type");
+		JLabel unitNumberLabel = new JLabel("Unit Number");
+		
+		
+		JComboBox jobAssignmentBox = new JComboBox();
+		JComboBox sleepingLocationBox = new JComboBox();
+		JComboBox healthFormBox = new JComboBox();
+		
+		final JTextField dateOfBirthField = new JTextField();
+		final JTextField emailAddressField = new JTextField();
+		final JTextField boyScoutIDField	= new JTextField();
+		final JTextField unitTypeField = new JTextField();
+		final JTextField unitNumberField = new JTextField();
+		
+		JButton acceptButton = new JButton("Accept");
+		JButton clearButton = new JButton("Clear");
+		
+		
+		personalPanel.add(jobAssignmentLabel);
+		personalPanel.add(jobAssignmentBox);
+		
+		personalPanel.add(sleepingLocationLabel);
+		personalPanel.add(sleepingLocationBox);
+		
+		personalPanel.add(healthFormLabel);
+		personalPanel.add(healthFormBox);
+		
+		personalPanel.add(verifyInfo);
+		personalPanel.add(dateOfBirthLabel);
+		personalPanel.add(dateOfBirthField);
+		
+		personalPanel.add(emailAddressLabel);
+		personalPanel.add(emailAddressField);
+		
+		personalPanel.add(boyScoutIDLabel);
+		personalPanel.add(boyScoutIDField);
+		
+		personalPanel.add(unitTypeLabel);
+		personalPanel.add(unitTypeField);
+		
+		personalPanel.add(unitNumberLabel);
+		personalPanel.add(unitNumberField);
+		
+		personalPanel.add(buttonPanel);
+		buttonPanel.add(acceptButton);
+		buttonPanel.add(clearButton);
+		
+		//Button action assignments
+		acceptButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO Change output function
+				System.out.println("Test1");
+				
+			}
+		});
+		clearButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			
+				dateOfBirthField.setText("");
+				emailAddressField.setText("");
+				boyScoutIDField.setText("");
+				unitNumberField.setText("");
+				unitTypeField.setText("");
+				
+				
+			}
+		});
 		
 		
 		//Payment
@@ -53,10 +139,10 @@ public class GuiMain {
 		
 		
 		//Edit Members
-		JFrame searchResults = new JFrame();
+		final JFrame searchResults = new JFrame();
 		JPanel buttonPanel2 = new JPanel();
 		JLabel infoPrompt = new JLabel("Please enter a name to search");
-		JTextField infoPromptField = new JTextField();
+		final JTextField infoPromptField = new JTextField();
 		JButton goButton = new JButton("Go");
 		JButton clearButton2 = new JButton("Clear");
 		JPanel searchResultsPanel = new JPanel(new GridLayout (0,1));
