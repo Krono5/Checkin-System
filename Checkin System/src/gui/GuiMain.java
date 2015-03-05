0a60ac7b963323b05dc81c10090fddb434735594
Alex <Alex@PrideRock.home>
2015-03-05 13:18:20 -0500

    Made some string arrays for date boxes.

--- Checkin System/src/gui/GuiMain.java
+++ Checkin System/src/gui/GuiMain.java
@@ -50,10 +50,19 @@ public class GuiMain {
 		JLabel nameLabel = new JLabel("Name");
 		JLabel oAIDLabel = new JLabel("Order of the Arrow ID");
 		
+		String[] dayArray = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
+		String[] monthArray = {"January","February", "March","April","May","June","July","August","September","October","November","December"};
+				
 		
 		JComboBox jobAssignmentBox = new JComboBox();
 		JComboBox sleepingLocationBox = new JComboBox();
 		JComboBox healthFormBox = new JComboBox();
+		JComboBox dayBox = new JComboBox(dayArray);
+		JComboBox monthBox = new JComboBox(monthArray);
+		JComboBox yearBox = new JComboBox();
+		
+		
+		
 		
 		final JTextField dateOfBirthField = new JTextField();
 		final JTextField emailAddressField = new JTextField();
